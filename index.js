e55ce4046018c1b353e6a98cc450c80b0e7ec605
Mohammed Musthafa AM <musthafaanjilakadavathu399@gmail.com>
2024-08-28 15:56:45 +0530

    New dice folder added

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var randomNumber1 = Math.floor((Math.random()*6)+1);
-var dice1_random = "images/dice" + randomNumber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", dice1_random);
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var dice1_random = "images/image1/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", dice1_random);
 
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var dice2_random = "images/image1/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", dice2_random);
 
-var randomNumber2 = Math.floor((Math.random()*6)+1);
-var dice2_random = "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", dice2_random);
-
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩Player 1 has Won!";
-}
-else if(randomNumber2>randomNumber1){
-    document.querySelector("h1").innerHTML = "🚩Player 2 has Won!";
-}
-else{
-    document.querySelector("h1").innerHTML = "Draw";
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩Dice 1 has Won!";
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "🚩Dice 2 has Won!";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw";
+    }
 }
